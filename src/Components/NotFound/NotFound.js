588ec8158c10434936759f449f2d1898cc7ab62e
Animesh Mondol <51692385+AnimeshMondol@users.noreply.github.com>
2022-04-04 21:58:11 +0600

    react router setup

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>404 not found.</h1>
+            <h3>There is currently no page using this name!!</h3>
+        </div>
+    );
+};
+
+export default NotFound;
